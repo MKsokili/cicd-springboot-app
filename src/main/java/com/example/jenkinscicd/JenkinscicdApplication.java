7cd7fef113ecc086345e680659911de7dd26842f
MKsokili <mksokili20@gmail.com>
2024-07-25 10:40:11 +0100

    test commit test

--- src/main/java/com/example/jenkinscicd/JenkinscicdApplication.java
+++ src/main/java/com/example/jenkinscicd/JenkinscicdApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinscicdApplication {
 	@GetMapping
 	public String message(){
-		return "welcome to jenkins test ci cd";
+		return "welcome to jenkins test ci cd test commit";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinscicdApplication.class, args);
