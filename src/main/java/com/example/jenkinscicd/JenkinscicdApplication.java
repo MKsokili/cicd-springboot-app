ba96835f3fa1b938434387aff15b7d33c9189641
MKsokili <mksokili20@gmail.com>
2024-07-29 10:01:41 +0100

    dzadaz

--- src/main/java/com/example/jenkinscicd/JenkinscicdApplication.java
+++ src/main/java/com/example/jenkinscicd/JenkinscicdApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinscicdApplication {
 	@GetMapping
 	public String message(){
-		return "welcome to jenkins test ci cd pipeline form jenkinsfile in repo , testing docker build image";
+		return "welcome to jenkins test ci cd pipeline form jenkinsfile in repo , testing docker build image zeza";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinscicdApplication.class, args);
