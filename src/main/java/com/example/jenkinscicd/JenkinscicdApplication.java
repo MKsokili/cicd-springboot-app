051d9c395fbad9e2c0d6033e9e2f5ee57e943d97
MKsokili <mksokili20@gmail.com>
2024-07-29 01:16:17 +0100

    build image from jenkinsfile

--- src/main/java/com/example/jenkinscicd/JenkinscicdApplication.java
+++ src/main/java/com/example/jenkinscicd/JenkinscicdApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinscicdApplication {
 	@GetMapping
 	public String message(){
-		return "welcome to jenkins test ci cd pipeline form jenkinsfile in repo";
+		return "welcome to jenkins test ci cd pipeline form jenkinsfile in repo , testing docker build image";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinscicdApplication.class, args);
